e670f502db8b4c1f9aa90dae69dc47df1cc4dbb7
OlegBondarUA <petrovichtrh@gmail.com>
2022-11-21 11:32:42 +0200

    fix bug state

--- bot.py
+++ bot.py
@@ -19,14 +19,16 @@ dp = Dispatcher(bot, storage=storage)
 
 
 class WeatherForm(StatesGroup):
-    city = State()
+    city_for_one_day_weather = State()
+    city_for_five_day_weather = State()
 
 
 @dp.message_handler(commands=['start'])
 async def Start_command(message: types.Message):
     kb = [
         [types.KeyboardButton(text="На 1 день")],
-        [types.KeyboardButton(text="На 5 днів")]
+        [types.KeyboardButton(text="На 5 днів")],
+        [types.KeyboardButton(text="Погодинна погода")]
         ]
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=kb,
@@ -39,213 +41,215 @@ async def Start_command(message: types.Message):
 
 @dp.message_handler(text='На 1 день')
 async def weather_1_day(message: types.Message):
-    await message.answer("Відмінний вибір!, Введи назву міста")
-    await WeatherForm.city.set()
+    await bot.send_message(message.chat.id, "Відмінний вибір!, Введи назву міста")
+    await WeatherForm.city_for_one_day_weather.set()
 
-    @dp.message_handler(state=WeatherForm.city)
-    async def get_weather(message: types.Message, state: FSMContext):
-        await state.update_data(city=message.text)
 
-        code_to_smile = {
-            'Clear': 'Ясно \U00002600',
-            'Clouds': 'Хмарно \U00002601',
-            'Rain': 'Дощ \U00002614',
-            'Drizzle': 'Дощ \U00002614',
-            'Thunderstorm': 'Сніг \U0001F328',
-            'Mist': 'Туман \U0001F32B'
-        }
+@dp.message_handler(state=WeatherForm.city_for_one_day_weather)
+async def get_weather(message: types.Message, state: FSMContext):
+    await state.update_data(city=message.text)
 
-        try:
-            r = requests.get(
-                f'https://api.openweathermap.org/data/2.5/weather?q='
-                f'{message.text}&appid={open_weather_token}&units=metric'
-            )
-            data = r.json()
+    code_to_smile = {
+        'Clear': 'Ясно \U00002600',
+        'Clouds': 'Хмарно \U00002601',
+        'Rain': 'Дощ \U00002614',
+        'Drizzle': 'Дощ \U00002614',
+        'Thunderstorm': 'Сніг \U0001F328',
+        'Mist': 'Туман \U0001F32B'
+    }
 
-            city = data['name']
-            cur_weather = data['main']['temp']
+    try:
+        r = requests.get(
+            f'https://api.openweathermap.org/data/2.5/weather?q='
+            f'{message.text}&appid={open_weather_token}&units=metric'
+        )
+        data = r.json()
 
-            weather_description = data['weather'][0]['main']
-            if weather_description in code_to_smile:
-                wd = code_to_smile[weather_description]
-            else:
-                wd = 'Глянь у вікно, не розумію що там за погода!'
-            humidity = data['main']['humidity']
-            pressure = data['main']['pressure']
-            wind = data['wind']['speed']
-            sunrise_timestamp = \
-                datetime.datetime.fromtimestamp(data['sys']['sunrise'])
-            sunset_timestamp = \
-                datetime.datetime.fromtimestamp(data['sys']['sunset'])
-            length_of_the_day = \
-                datetime.datetime.fromtimestamp(data['sys']['sunset']) - \
-                datetime.datetime.fromtimestamp(data['sys']['sunrise'])
+        city = data['name']
+        cur_weather = data['main']['temp']
 
-            await message.reply(
-                f'###{datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}###\n'
-                f'Погода в місті: {city}\nТемпература: {cur_weather}C° {wd}\n'
-                f'Вологість: {humidity}%\nТиск: {pressure} мм.рт.ст\n'
-                f'Вітер: {wind} м/с\nСхід сонця: {sunrise_timestamp}\n'
-                f'Захід сонця: {sunset_timestamp}\n'
-                f'Тривалість дня: {length_of_the_day}\n'
-                f'Гарного дня!', reply_markup=types.ReplyKeyboardRemove())
-            await state.finish()
-        except Exception as error:
-            print(error)
-            await message.reply('\U00002620Перевірте назву міста\U00002620')
+        weather_description = data['weather'][0]['main']
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = 'Глянь у вікно, не розумію що там за погода!'
+        humidity = data['main']['humidity']
+        pressure = data['main']['pressure']
+        wind = data['wind']['speed']
+        sunrise_timestamp = \
+            datetime.datetime.fromtimestamp(data['sys']['sunrise'])
+        sunset_timestamp = \
+            datetime.datetime.fromtimestamp(data['sys']['sunset'])
+        length_of_the_day = \
+            datetime.datetime.fromtimestamp(data['sys']['sunset']) - \
+            datetime.datetime.fromtimestamp(data['sys']['sunrise'])
+
+        await message.reply(
+            f'###{datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}###\n'
+            f'Погода в місті: {city}\nТемпература: {cur_weather}C° {wd}\n'
+            f'Вологість: {humidity}%\nТиск: {pressure} мм.рт.ст\n'
+            f'Вітер: {wind} м/с\nСхід сонця: {sunrise_timestamp}\n'
+            f'Захід сонця: {sunset_timestamp}\n'
+            f'Тривалість дня: {length_of_the_day}\n'
+            f'Гарного дня!', reply_markup=types.ReplyKeyboardRemove())
+        await state.finish()
+    except Exception as error:
+        print(error)
+        await message.reply('\U00002620Перевірте назву міста\U00002620')
 
 
 @dp.message_handler(text='На 5 днів')
 async def weather_5_day(message: types.Message):
-    await message.answer("Введи назву міста")
-    await WeatherForm.city.set()
+    await message.reply("Введи назву міста")
+    await WeatherForm.city_for_five_day_weather.set()
 
-    @dp.message_handler(state=WeatherForm.city)
-    async def get_weathers(message: types.Message, state: FSMContext):
-        await state.update_data(city=message.text)
 
-        code_to_smile = {
-            'Clear': 'Ясно \U00002600',
-            'Clouds': 'Хмарно \U00002601',
-            'Overcast clouds': 'Похмурі хмари \U00002601',
-            'Rain': 'Дощ \U00002614',
-            'Light rain': 'Невеликий дощ \U00002614',
-            'Drizzle': 'Дощ \U00002614',
-            'Thunderstorm': 'Сніг \U0001F328',
-            'Snow': 'Сніг \U0001F328',
-            'Light snow': 'Невеликий сніг \U0001F328',
-            'Mist': 'Туман \U0001F32B',
-            'Light mist': 'Туман \U0001F32B'
+@dp.message_handler(state=WeatherForm.city_for_five_day_weather)
+async def get_weathers(message: types.Message, state: FSMContext):
+    await state.update_data(city=message.text)
+
+    code_to_smile = {
+        'Clear': 'Ясно \U00002600',
+        'Clouds': 'Хмарно \U00002601',
+        'Overcast clouds': 'Похмурі хмари \U00002601',
+        'Rain': 'Дощ \U00002614',
+        'Light rain': 'Невеликий дощ \U00002614',
+        'Drizzle': 'Дощ \U00002614',
+        'Thunderstorm': 'Сніг \U0001F328',
+        'Snow': 'Сніг \U0001F328',
+        'Light snow': 'Невеликий сніг \U0001F328',
+        'Mist': 'Туман \U0001F32B',
+        'Light mist': 'Туман \U0001F32B'
+    }
+
+    try:
+        request = requests.get(
+            f'https://api.openweathermap.org/data/2.5/forecast?q='
+            f'{message.text}&appid={open_weather_token}&units=metric')
+        data = request.json()
+
+        day_1 = {
+            'city': data['city']['name'],
+            'date': str(
+                datetime.datetime.fromtimestamp(data['list'][0]['dt']))[
+                    :10],
+            'humidity': data['list'][0]['main']['humidity'],
+            'pressure': data['list'][0]['main']['pressure'],
+            'temp': data['list'][0]['main']['temp'],
+            'weather': data['list'][0]['weather'][0]['main'],
+            'wind': data['list'][0]['wind']['speed'],
+            'visibility': data['list'][0]['visibility'],
         }
 
-        try:
-            request = requests.get(
-                f'https://api.openweathermap.org/data/2.5/forecast?q='
-                f'{message.text}&appid={open_weather_token}&units=metric')
-            data = request.json()
+        if day_1['weather'] in code_to_smile:
+            wd = code_to_smile[day_1['weather']]
+        else:
+            wd = 'Глянь у вікно, не розумію що там за погода!'
 
-            day_1 = {
-                'city': data['city']['name'],
-                'date': str(
-                    datetime.datetime.fromtimestamp(data['list'][0]['dt']))[
-                        :10],
-                'humidity': data['list'][0]['main']['humidity'],
-                'pressure': data['list'][0]['main']['pressure'],
-                'temp': data['list'][0]['main']['temp'],
-                'weather': data['list'][0]['weather'][0]['main'],
-                'wind': data['list'][0]['wind']['speed'],
-                'visibility': data['list'][0]['visibility'],
-            }
+        day_2 = {
+            'city': data['city']['name'],
+            'date': str(datetime.datetime.fromtimestamp(
+                data['list'][8]['dt']))[:10],
+            'humidity': data['list'][8]['main']['humidity'],
+            'pressure': data['list'][8]['main']['pressure'],
+            'temp': data['list'][8]['main']['temp'],
+            'weather': data['list'][8]['weather'][0]['main'],
+            'wind': data['list'][8]['wind']['speed'],
+            'visibility': data['list'][8]['visibility'],
+        }
 
-            if day_1['weather'] in code_to_smile:
-                wd = code_to_smile[day_1['weather']]
-            else:
-                wd = 'Глянь у вікно, не розумію що там за погода!'
+        if day_2['weather'] in code_to_smile:
+            wd_2 = code_to_smile[day_1['weather']]
+        else:
+            wd_2 = 'Глянь у вікно, не розумію що там за погода!'
 
-            day_2 = {
-                'city': data['city']['name'],
-                'date': str(datetime.datetime.fromtimestamp(
-                    data['list'][8]['dt']))[:10],
-                'humidity': data['list'][8]['main']['humidity'],
-                'pressure': data['list'][8]['main']['pressure'],
-                'temp': data['list'][8]['main']['temp'],
-                'weather': data['list'][8]['weather'][0]['main'],
-                'wind': data['list'][8]['wind']['speed'],
-                'visibility': data['list'][8]['visibility'],
-            }
+        day_3 = {
+            'city': data['city']['name'],
+            'date': str(datetime.datetime.fromtimestamp(
+                data['list'][16]['dt']))[:10],
+            'humidity': data['list'][16]['main']['humidity'],
+            'pressure': data['list'][16]['main']['pressure'],
+            'temp': data['list'][16]['main']['temp'],
+            'weather': data['list'][16]['weather'][0]['main'],
+            'wind': data['list'][16]['wind']['speed'],
+            'visibility': data['list'][16]['visibility'],
+        }
 
-            if day_2['weather'] in code_to_smile:
-                wd_2 = code_to_smile[day_1['weather']]
-            else:
-                wd_2 = 'Глянь у вікно, не розумію що там за погода!'
+        if day_3['weather'] in code_to_smile:
+            wd_3 = code_to_smile[day_1['weather']]
+        else:
+            wd_3 = 'Глянь у вікно, не розумію що там за погода!'
 
-            day_3 = {
-                'city': data['city']['name'],
-                'date': str(datetime.datetime.fromtimestamp(
-                    data['list'][16]['dt']))[:10],
-                'humidity': data['list'][16]['main']['humidity'],
-                'pressure': data['list'][16]['main']['pressure'],
-                'temp': data['list'][16]['main']['temp'],
-                'weather': data['list'][16]['weather'][0]['main'],
-                'wind': data['list'][16]['wind']['speed'],
-                'visibility': data['list'][16]['visibility'],
-            }
+        day_4 = {
+            'city': data['city']['name'],
+            'date': str(datetime.datetime.fromtimestamp(
+                data['list'][24]['dt']))[:10],
+            'humidity': data['list'][24]['main']['humidity'],
+            'pressure': data['list'][24]['main']['pressure'],
+            'temp': data['list'][24]['main']['temp'],
+            'weather': data['list'][24]['weather'][0]['main'],
+            'wind': data['list'][24]['wind']['speed'],
+            'visibility': data['list'][24]['visibility'],
+        }
 
-            if day_3['weather'] in code_to_smile:
-                wd_3 = code_to_smile[day_1['weather']]
-            else:
-                wd_3 = 'Глянь у вікно, не розумію що там за погода!'
+        if day_4['weather'] in code_to_smile:
+            wd_4 = code_to_smile[day_1['weather']]
+        else:
+            wd_4 = 'Глянь у вікно, не розумію що там за погода!'
 
-            day_4 = {
-                'city': data['city']['name'],
-                'date': str(datetime.datetime.fromtimestamp(
-                    data['list'][24]['dt']))[:10],
-                'humidity': data['list'][24]['main']['humidity'],
-                'pressure': data['list'][24]['main']['pressure'],
-                'temp': data['list'][24]['main']['temp'],
-                'weather': data['list'][24]['weather'][0]['main'],
-                'wind': data['list'][24]['wind']['speed'],
-                'visibility': data['list'][24]['visibility'],
-            }
+        day_5 = {
+            'city': data['city']['name'],
+            'date': str(datetime.datetime.fromtimestamp(
+                data['list'][32]['dt']))[:10],
+            'humidity': data['list'][32]['main']['humidity'],
+            'pressure': data['list'][32]['main']['pressure'],
+            'temp': data['list'][32]['main']['temp'],
+            'weather': data['list'][32]['weather'][0]['main'],
+            'wind': data['list'][32]['wind']['speed'],
+            'visibility': data['list'][32]['visibility'],
+        }
 
-            if day_4['weather'] in code_to_smile:
-                wd_4 = code_to_smile[day_1['weather']]
-            else:
-                wd_4 = 'Глянь у вікно, не розумію що там за погода!'
+        if day_5['weather'] in code_to_smile:
+            wd_5 = code_to_smile[day_1['weather']]
+        else:
+            wd_5 = 'Глянь у вікно, не розумію що там за погода!'
 
-            day_5 = {
-                'city': data['city']['name'],
-                'date': str(datetime.datetime.fromtimestamp(
-                    data['list'][32]['dt']))[:10],
-                'humidity': data['list'][32]['main']['humidity'],
-                'pressure': data['list'][32]['main']['pressure'],
-                'temp': data['list'][32]['main']['temp'],
-                'weather': data['list'][32]['weather'][0]['main'],
-                'wind': data['list'][32]['wind']['speed'],
-                'visibility': data['list'][32]['visibility'],
-            }
-
-            if day_5['weather'] in code_to_smile:
-                wd_5 = code_to_smile[day_1['weather']]
-            else:
-                wd_5 = 'Глянь у вікно, не розумію що там за погода!'
-
-            await message.reply(
-                f'###{day_1["date"]}###\n'
-                f'Погода в місті: {day_1["city"]}\nТемпература: '
-                f'{day_1["temp"]}C° {wd}\n Вологість: {day_1["humidity"]}%\n'
-                f'Тиск: {day_1["pressure"]} мм.рт.ст\n '
-                f'Вітер: {day_1["wind"]} м/с\n '
-                f'Видимість: {day_1["visibility"]} м\n\n'
-                f'###{day_2["date"]}###\n'
-                f'Погода в місті: {day_2["city"]}\nТемпература: '
-                f'{day_2["temp"]}C° {wd_2}\n Вологість: {day_2["humidity"]}%\n'
-                f'Тиск: {day_2["pressure"]} мм.рт.ст\n'
-                f'Вітер: {day_2["wind"]} м/с\n'
-                f'Видимість: {day_2["visibility"]}\n\n'
-                f'###{day_3["date"]}###\n'
-                f'Погода в місті: {day_3["city"]}\nТемпература: '
-                f'{day_3["temp"]}C° {wd_3}\n Вологість: {day_3["humidity"]}%\n'
-                f'Тиск: {day_3["pressure"]} мм.рт.ст\n'
-                f'Вітер: {day_3["wind"]} м/с\n'
-                f'Видимість: {day_3["visibility"]}\n\n'
-                f'###{day_4["date"]}###\n'
-                f'Погода в місті: {day_4["city"]}\nТемпература: '
-                f'{day_4["temp"]}C° {wd_4}\n Вологість: {day_4["humidity"]}%\n'
-                f'Тиск: {day_4["pressure"]} мм.рт.ст\n'
-                f'Вітер: {day_4["wind"]} м/с\n'
-                f'Видимість: {day_4["visibility"]}\n\n'
-                f'###{day_5["date"]}###\n'
-                f'Погода в місті: {day_5["city"]}\nТемпература: '
-                f'{day_5["temp"]}C° {wd_5}\n Вологість: {day_5["humidity"]}%\n'
-                f'Тиск: {day_5["pressure"]} мм.рт.ст\n'
-                f'Вітер: {day_5["wind"]} м/с\n'
-                f'Видимість: {day_5["visibility"]}\n\n'
-                f'Гарного дня!', reply_markup=types.ReplyKeyboardRemove())
-            await state.finish()
-        except Exception as error:
-            print(error)
-            await message.reply('\U00002620Перевірте назву міста\U00002620')
+        await message.reply(
+            f'###{day_1["date"]}###\n'
+            f'Погода в місті: {day_1["city"]}\nТемпература: '
+            f'{day_1["temp"]}C° {wd}\n Вологість: {day_1["humidity"]}%\n'
+            f'Тиск: {day_1["pressure"]} мм.рт.ст\n '
+            f'Вітер: {day_1["wind"]} м/с\n '
+            f'Видимість: {day_1["visibility"]} м\n\n'
+            f'###{day_2["date"]}###\n'
+            f'Погода в місті: {day_2["city"]}\nТемпература: '
+            f'{day_2["temp"]}C° {wd_2}\n Вологість: {day_2["humidity"]}%\n'
+            f'Тиск: {day_2["pressure"]} мм.рт.ст\n'
+            f'Вітер: {day_2["wind"]} м/с\n'
+            f'Видимість: {day_2["visibility"]}\n\n'
+            f'###{day_3["date"]}###\n'
+            f'Погода в місті: {day_3["city"]}\nТемпература: '
+            f'{day_3["temp"]}C° {wd_3}\n Вологість: {day_3["humidity"]}%\n'
+            f'Тиск: {day_3["pressure"]} мм.рт.ст\n'
+            f'Вітер: {day_3["wind"]} м/с\n'
+            f'Видимість: {day_3["visibility"]}\n\n'
+            f'###{day_4["date"]}###\n'
+            f'Погода в місті: {day_4["city"]}\nТемпература: '
+            f'{day_4["temp"]}C° {wd_4}\n Вологість: {day_4["humidity"]}%\n'
+            f'Тиск: {day_4["pressure"]} мм.рт.ст\n'
+            f'Вітер: {day_4["wind"]} м/с\n'
+            f'Видимість: {day_4["visibility"]}\n\n'
+            f'###{day_5["date"]}###\n'
+            f'Погода в місті: {day_5["city"]}\nТемпература: '
+            f'{day_5["temp"]}C° {wd_5}\n Вологість: {day_5["humidity"]}%\n'
+            f'Тиск: {day_5["pressure"]} мм.рт.ст\n'
+            f'Вітер: {day_5["wind"]} м/с\n'
+            f'Видимість: {day_5["visibility"]}\n\n'
+            f'Гарного дня!', reply_markup=types.ReplyKeyboardRemove())
+        await state.finish()
+    except Exception as error:
+        print(error)
+        await message.reply('\U00002620Перевірте назву міста\U00002620')
 
 
 if __name__ == '__main__':
